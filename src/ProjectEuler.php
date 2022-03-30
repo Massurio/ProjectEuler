7b4e24e5754c82b6a87439440c96309a5774ad29
Massurio <massurio@gmail.com>
2022-03-30 13:06:40 +0100

    RemoveComment

--- src/ProjectEuler.php
+++ src/ProjectEuler.php
@@ -6,95 +6,92 @@ class ProjectEuler
 {
     public function eulerSix($number)
     {
-        $sum1 = 0;
-        $sum2 = 0;
-
+        $sumOfSquares = 0;
+        $squareOfSums = 0;
         for ($i = 1; $i <= $number; $i++) {
-        $sum1 += ($i * $i);
-        $sum2 += $i;
+            $sumOfSquares += ($i * $i);
+            $squareOfSums += $i;
         }
-
-        $sum2 *= $sum2;
-
-        return $sum2 - $sum1;
+        $squareOfSums *= $squareOfSums;
+        return $squareOfSums - $sumOfSquares;
     }
 
+    public function eulerFive($input)
+    {
+        $sum = 1;
+        $off = false;
+        while (!$off) {
+            $count = 0;
+            for ($x = 1; $x <= $input; $x++) {
+                if ($sum % $x != 0) {
+                    $count += 1;
+                    break;
+                }
+            }
+            if ($count == 0) {
+                return $sum;
+                $off = true;
+            }
+            $sum++;
+        }
+    }
 
-//    public function eulerFive($input)
-//    {
-//        $sum = 1;
-//        $off = false;
-//        while (!$off) {
-//            $count = 0;
-//            for ($x = 1; $x <= $input; $x++) {
-//                if ($sum % $x != 0) {
-//                    $count += 1;
-//                    break;
-//                }
-//            }
-//            if ($count == 0) {
-//                return $sum;
-//                $off = true;
-//            }
-//            $sum++;
-//        }
-//    }
-//
-//    public function eulerFour()
-//    {
-//        $palindrome = 0;
-//
-//        for ($i = 100; $i <= 999; $i++) {
-//            for ($y = 100; $y <= 999; $y++) {
-//                $x = $i * $y;
-//
-//                if ($x > $palindrome && $x == strrev($x)) {
-//                    $palindrome = $x;
-//                }
-//            }
-//        }
-//        return $palindrome;
-//    }
-//
-//    public function eulerThree($number)
-//    {
-//        $prime = 2;
-//        while ($prime * $prime < $number) {
-//            while ($number % $prime === 0) {
-//                $number /= $prime;
-//            }
-//            $prime++;
-//        }
-//        return $number;
-//    }
-//
-//    public function eulerTwo($max_value)
-//    {
-//        $sequence = [1,1,2];
-//        $sum = 0;
-//
-//        while ($sequence[2] < $max_value) {
-//            if ($sequence[2] % 2 === 0) {
-//                $sum += $sequence[2];
-//            }
-//            $sequence[0] = $sequence[1];
-//            $sequence[1] = $sequence[2];
-//            $sequence[2] = $sequence[0] + $sequence[1];
-//        }
-//        return $sum;
-//    }
-//
-//    public function eulerOne($limit)
-//    {
-//        $result = 0;
-//        $number = 1;
-//
-//        while ($number < $limit) {
-//            if ($number % 3 === 0 || $number % 5 === 0) {
-//                $result +=  $number;
-//            }
-//            $number++;
-//        }
-//        return $result;
-//    }
+    public function eulerFour()
+    {
+        $palindrome = 0;
+
+        for ($i = 100; $i <= 999; $i++) {
+            for ($y = 100; $y <= 999; $y++) {
+                $x = $i * $y;
+
+                if ($x > $palindrome && $x == strrev($x)) {
+
+                    $palindrome = $x;
+                }
+            }
+        }
+        return $palindrome;
+    }
+
+    public function eulerThree($number)
+    {
+        $prime = 2;
+        while ($prime * $prime < $number) {
+            while ($number % $prime === 0) {
+                $number /= $prime;
+            }
+            $prime++;
+        }
+        return $number;
+    }
+
+    public function eulerTwo($max_value)
+    {
+        $sequence = [1,1,2];
+        $sum = 0;
+
+        while ($sequence[2] < $max_value) {
+            if ($sequence[2] % 2 === 0) {
+                $sum += $sequence[2];
+            }
+            $sequence[0] = $sequence[1];
+            $sequence[1] = $sequence[2];
+            $sequence[2] = $sequence[0] + $sequence[1];
+        }
+        return $sum;
+    }
+
+    public function eulerOne($limit)
+    {
+        $result = 0;
+        $number = 1;
+
+        while ($number < $limit) {
+            if ($number % 3 === 0 || $number % 5 === 0) {
+                $result +=  $number;
+            }
+            $number++;
+        }
+        return $result;
+    }
 }
---
+++ tests/OneTest/OneTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace OneTest;
+
+use Code\ProjectEuler;
+use PHPUnit\Framework\TestCase;
+
+class OneTest extends TestCase
+{
+    public function testSixResult()
+    {
+        $euler = new ProjectEuler();
+        $out = $euler->eulerSix(100);
+        $this->assertSame(25164150, $out);
+    }
+
+    public function testSix()
+    {
+        $euler = new ProjectEuler();
+        $out = $euler->eulerSix(10);
+        $this->assertSame(2640, $out);
+    }
+
+
+    public function testFiveResult()
+    {
+        $euler = new ProjectEuler();
+        $out = $euler->eulerFive(20);
+        $this->assertSame(232792560, $out);
+    }
+
+    public function testFive()
+    {
+        $euler = new ProjectEuler();
+        $out = $euler->eulerFive(10);
+        $this->assertSame(2520, $out);
+    }
+
+    public function testFour()
+    {
+        $euler = new ProjectEuler();
+        $out = $euler->eulerFour();
+        $this->assertSame(906609, $out);
+    }
+
+    public function testThree()
+    {
+        $euler = new ProjectEuler();
+        $out = $euler->eulerThree(600851475143);
+        $this->assertSame(6857, $out);
+    }
+
+    public function testTwo()
+    {
+        $euler = new ProjectEuler();
+        $out = $euler->eulerTwo(4000000);
+        $this->assertSame(4613732, $out);
+    }
+
+    public function testEulerResult()
+    {
+        $euler = new ProjectEuler();
+        $out = $euler->eulerOne(1000);
+        $this->assertSame(233168, $out);
+    }
+}
